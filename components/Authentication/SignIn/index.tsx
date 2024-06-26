8ed22310abc2ae8dd5419abb702e8bcdc2b98363
Adrian Trifonov <adrian.trifonov@gmail.com>
2024-06-26 14:35:29 -0400

    Finished the login form but has centering issues

--- components/Authentication/SignIn/index.tsx
+++ components/Authentication/SignIn/index.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { signIn } from "@/auth"
-import { Button, Center, Checkbox, Container, Flex, Group, Paper, TextInput } from '@mantine/core';
+import { Button, Center, Checkbox, Container, Flex, Group, Paper, TextInput, Title } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import classes from './SignIn.module.css';
 import { useState } from "react";
@@ -11,6 +11,7 @@ export function SignIn() {
     mode: 'uncontrolled',
     initialValues: {
       email: '',
+      password: '',
       termsOfService: false,
     },
 
@@ -20,35 +21,52 @@ export function SignIn() {
   });
 
   const [emailFocused, setEmailFocused] = useState(false);
-  const [emailValue, setEmailValue] = useState('');
-  const emailFloating = emailValue.trim().length !== 0 || emailFocused || undefined;
+  const emailFloating = form.values.email.trim().length !== 0 || emailFocused || undefined;
+
+  const [passwordFocused, setPasswordFocused] = useState(false);
+  const passwordFloating = form.values.password.trim().length !== 0 || passwordFocused || undefined;
 
   return (
-    <Container size="md">
+    <>
       <Center>
-        <Flex>
+        <Title order={1}>Panel authentication</Title>
+      </Center>
+        <Center>
           <div className={classes.inner}>
             <div className={classes.content}>
-              <Paper shadow="xs" radius="md" withBorder p="30px">
+              <Paper shadow="xs" radius="md" withBorder p="30px" style={{width: "200%"}}>
                 <form onSubmit={form.onSubmit((values) => console.log(values))}>
                   <TextInput
                     withAsterisk
                     label="Email"
                     required
+                    classNames={classes}
                     data-floating={emailFloating}
                     labelProps={{ 'data-floating': emailFloating }}
-                    placeholder="johndoe@example.com"
+                    placeholder="johndoe@example.com"          
                     key={form.key('email')}
-                    {...form.getInputProps('email')}
+                    mt="xs"
                     onFocus={() => setEmailFocused(true)}
                     onBlur={() => setEmailFocused(false)}
+                    {...form.getInputProps('email', {
+                      withFocus: false,
+                    })}
                   />
                   <TextInput
                     withAsterisk
                     label="Password"
+                    required
+                    classNames={classes}
+                    data-floating={passwordFloating}
+                    labelProps={{ 'data-floating': passwordFloating }}
+                    mt="xl"
                     placeholder="1L0v3P@ssw0rds"
-                    key={form.key('email')}
-                    {...form.getInputProps('email')}
+                    onFocus={() => setPasswordFocused(true)}
+                    onBlur={() => setPasswordFocused(false)}
+                    key={form.key('password')}
+                    {...form.getInputProps('email', {
+                      withFocus: false,
+                    })}
                   />
 
                   <Checkbox
@@ -59,14 +77,13 @@ export function SignIn() {
                   />
 
                   <Group justify="center" mt="md">
-                    <Button type="submit">Submit</Button>
+                    <Button type="submit" style={{ width: "100%" }} radius="md" size="md">Submit</Button>
                   </Group>
                 </form>
               </Paper>
             </div>
           </div>
-        </Flex>
-      </Center>
-    </Container>
+        </Center>
+    </>
   );
 }
