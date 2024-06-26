0c7cdda14f5afa0cf5f2bff827afe3ac283d0578
Adrian Trifonov <adrian.trifonov@gmail.com>
2024-06-26 11:26:26 -0400

    Removed ColorSchemeToggle and added the navbar, no logo yet so using the MantineLogo as a placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Navbar } from '../components/layout/Navbar';
 
 export default function HomePage() {
   return (
     <>
+      <Navbar />
       <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
