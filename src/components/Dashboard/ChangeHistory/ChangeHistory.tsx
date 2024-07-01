be7b87651946b9e576146c480851e1d3de0bae39
Adrian Trifonov <adrian.trifonov@gmail.com>
2024-07-01 14:21:59 -0400

    Made it buildable

--- src/components/Dashboard/ChangeHistory/ChangeHistory.tsx
+++ src/components/Dashboard/ChangeHistory/ChangeHistory.tsx
@@ -4,14 +4,12 @@ import { Card, Title } from '@mantine/core';
 import classes from '../Dashboard.module.css';
 
 export function ChangeHistory() {
-
  return (
   <Card radius="md">
    <Card.Section className={classes.section}>
     <Title order={5}>Latest Block</Title>
    </Card.Section>
    <Card.Section className={classes.section}>
-    <MRT_Table table={table} />
    </Card.Section>
   </Card>
  );
